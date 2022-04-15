357955f1c5857524aa88b2e94e1e412ada9ec3d8
iuemon83 <83.program+github@gmail.com>
2022-04-15 14:12:20 +0000

    update

--- components/CardSetDataViewerForm.tsx
+++ components/CardSetDataViewerForm.tsx
@@ -8,25 +8,17 @@ interface Props {
 
 const CardSetDataViewerForm: React.FC<Props> = (props: Props) => {
   const { cardset } = props;
-  const data = [
-    { name: "Page A", uv: 400, pv: 2400, amt: 2400 },
-    { name: "Page B", uv: 300, pv: 2400, amt: 2400 },
-    { name: "Page C", uv: 300, pv: 2400, amt: 2400 },
-    { name: "Page D", uv: 200, pv: 2400, amt: 2400 },
-    { name: "Page E", uv: 300, pv: 2400, amt: 2400 },
-    { name: "Page F", uv: 200, pv: 2400, amt: 2400 },
-  ];
 
   const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
 
-  const byCost: number[] = [];
+  const byCost: { [type: number]: { cost: number; numOfCards: number } } = {};
   const byCardType: { [type: string]: { type: string; numOfCards: number } } = {};
 
   cardset.cards.forEach((card) => {
     if (byCost[card.cost] === undefined) {
-      byCost[card.cost] = 0;
+      byCost[card.cost] = { cost: card.cost, numOfCards: 0 };
     }
-    byCost[card.cost]++;
+    byCost[card.cost].numOfCards++;
 
     if (byCardType[card.type] === undefined) {
       byCardType[card.type] = { type: card.type, numOfCards: 0 };
@@ -34,9 +26,8 @@ const CardSetDataViewerForm: React.FC<Props> = (props: Props) => {
     byCardType[card.type].numOfCards++;
   });
 
-  const byCostData = byCost.map((x, i) => {
-    return { cost: i, numOfCards: x };
-  });
+  const byCostData = Object.values(byCost);
+  console.log(byCostData);
 
   const byCardTypeData = Object.values(byCardType);
   console.log(byCardTypeData);
@@ -67,7 +58,7 @@ const CardSetDataViewerForm: React.FC<Props> = (props: Props) => {
             return `${x.type} ${(x.percent * 100).toFixed(0)}%`;
           }}
         >
-          {data.map((entry, index) => (
+          {byCardTypeData.map((entry, index) => (
             <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
           ))}
         </Pie>
