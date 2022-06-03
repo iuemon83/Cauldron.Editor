217ab9154a45fb9bbb7a1c1c6d042f9a23bdb083
iuemon83 <83.program+github@gmail.com>
2022-06-03 18:18:46 +0000

    indexページをカードセットのページに変更する #50

--- next.config.js
+++ next.config.js
@@ -2,28 +2,46 @@ const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 const urlPrefix = process.env.URL_PREFIX ? "/" + process.env.URL_PREFIX : "";
 
 module.exports = (phase, { defaultConfig }) => {
+  const c = {
+    async redirects() {
+      return [
+        {
+          source: "/",
+          destination: "/about",
+          permanent: true,
+        },
+      ];
+    },
+  };
+
   if (phase === PHASE_DEVELOPMENT_SERVER) {
     return {
-      /* development only config options here */
-      webpack: (config) => {
-        config.watchOptions = {
-          aggregateTimeout: 200,
-          poll: 1000,
-          ignored: /node_modules/,
-        };
-        return config;
+      ...d,
+      ...{
+        /* development only config options here */
+        webpack: (config) => {
+          config.watchOptions = {
+            aggregateTimeout: 200,
+            poll: 1000,
+            ignored: /node_modules/,
+          };
+          return config;
+        },
       },
     };
   }
 
   return {
-    /* config options for all phases except development here */
-    assetPrefix: urlPrefix,
-    experimental: {
-      outputStandalone: true,
-    },
-    publicRuntimeConfig: {
-      basePath: urlPrefix,
+    ...d,
+    ...{
+      /* config options for all phases except development here */
+      assetPrefix: urlPrefix,
+      experimental: {
+        outputStandalone: true,
+      },
+      publicRuntimeConfig: {
+        basePath: urlPrefix,
+      },
     },
   };
 };
